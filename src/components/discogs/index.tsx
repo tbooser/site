cf95db4d58241af284917b058f8cd42e9289cee1
Timothy Booser <timothy.booser@time.com>
2022-10-22 11:39:16 -0400

    fix: comment out unused

--- src/components/discogs/index.tsx
+++ src/components/discogs/index.tsx
@@ -1,16 +1,17 @@
-import { useContext } from 'react'
-import {
-  DiscogsDataContext,
-  DiscogsDataReturnTypes,
-} from '../../context/DiscogsContext/DiscogsContext'
+// import { useContext } from 'react'
+// import {
+//   DiscogsDataContext,
+//   DiscogsDataReturnTypes,
+// } from '../../context/DiscogsContext/DiscogsContext'
 
 const Discogs = () => {
-  const discogsContext = useContext(
-    DiscogsDataContext,
-  ) as DiscogsDataReturnTypes
-  const { collectionSize } = discogsContext
+  // const discogsContext = useContext(
+  //   DiscogsDataContext,
+  // ) as DiscogsDataReturnTypes
+  // const { collectionSize } = discogsContext
 
-  return <div>Discogs {collectionSize}</div>
+  // return <div>Discogs {collectionSize}</div>
+  return <div>Discogs</div>
 }
 
 export default Discogs
